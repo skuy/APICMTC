aa604aa495b6e870eeb28d1fc44732ae3024c910
mac <thanakorn940@gmail.com>
2016-08-05 15:55:03 +0700

    First

---
+++ request.php
@@ -0,0 +1,17 @@
+<?php
+
+    $data = array('api' => 'API123456','student_id' => '5839010012','birthday' => '26/05/2539');
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'http://it.cmtc.ac.th/lab/api.php');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+    $exec = curl_exec($curl);
+    curl_close($curl);
+
+    //echo $exec;
+    
+    var_dump(json_decode($exec));
+?>
